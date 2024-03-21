f9202e664899119e7e0e424d05063961f44c3cb5
Millan125 <andyjones12722@gmail.com>
2024-03-20 20:16:29 -0400

    'created update_master.py to go through wsr_week_to_path removing kwargs as we go and add the corresponding data to master_df also addded the week keyword to the df in the week column'

--- read_excel_write_df.py
+++ read_excel_write_df.py
@@ -1,7 +1,7 @@
-def main(sales_report):
+def rewd(key, path):
     import pandas as pd
 
-    df = pd.read_excel(r"" + sales_report, usecols="D:Q")
+    df = pd.read_excel(r"" + path, usecols="D:Q")
 
     final_df = df.loc[[7, 8, 29]]
 
@@ -9,7 +9,9 @@ def main(sales_report):
 
     unique_dates = pd.to_datetime(df_filled.iloc[0].unique(), format="%m/%d/%Y")
 
-    final_df = pd.DataFrame(index=unique_dates, columns=["AM", "PM", "Total", "Note"])
+    final_df = pd.DataFrame(
+        index=unique_dates, columns=["Week", "AM", "PM", "Total", "Note"]
+    )
 
     am_filtered_df = df_filled.loc[:, df_filled.iloc[1] == "AM"].copy()
     am_filtered_df.replace(",", "", regex=True, inplace=True)
@@ -25,10 +27,12 @@ def main(sales_report):
 
     final_df["Total"] = final_df["AM"] + final_df["PM"]
 
+    final_df["Week"] = key
+
     return final_df
 
 
 if __name__ == "__main__":
-    main(
+    rewd(
         "C:\\Users\\milla\\Downloads\\Weekly Sales Report - 0215 - Forbes Ave, Pittsburgh, PA   - Week 51, 2023.xls"
     )
---
+++ testing.py
@@ -0,0 +1,7 @@
+my_dict = {"a": 1, "b": 2, "c": 3}
+
+# Return and remove a key-value pair using popitem()
+popped_item = my_dict.popitem()
+
+print("Popped item:", popped_item)
+print("Updated dictionary:", my_dict)
