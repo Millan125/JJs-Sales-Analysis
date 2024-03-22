a44651b1696fd5231f7b3a1f30c272b1c3a3a6b9
Millan125 <andyjones12722@gmail.com>
2024-03-22 16:54:12 -0400

    "changed the update_master to not empty the wsr_week_to_path json, and update_master no longer reads the master_sales file. instead it creates a new xlsx file from the json then overwrites the old xlsx file each time"

--- update_master.py
+++ update_master.py
@@ -11,25 +11,25 @@ def um():
 
     # with open("master_sales.xlsx", "r") as f:
     #     master_xls = f.read()
-    if os.path.getsize("master_sales.xlsx") != 0:
-        master_df = pd.read_excel("master_sales.xlsx")
-    else:
-        master_df = pd.DataFrame()
+    # if os.path.getsize("master_sales.xlsx") != 0:
+    #     master_df = pd.read_excel("master_sales.xlsx", engine="openpyxl")
+    # else:
+    master_df = pd.DataFrame()
 
     while path_dict:
         path = path_dict.popitem()
         new_df = rewd(path[0], path[1])
-        new_master_df = pd.concat([master_df, new_df]).sort_index(inplace=False)
+        master_df = pd.concat([master_df, new_df]).sort_index(inplace=False)
         print(path)
         # print(new_df.head())
-        print(new_master_df.head())
+        print(master_df.head())
     # with open("wsr_week_to_path.json", "w") as f:
     #     pass
 
-    new_master_df.to_excel("master_sales.xlsx", index=False)
+    master_df.to_excel("master_sales.xlsx", index=True)
 
-    with open("master_sales.xlsx", "w") as f:
-        f.write(pd.to_csv(new_master_df))
+    # with open("master_sales.xlsx", "w") as f:
+    #     f.write(pd.to_csv(master_df))
 
 
 if __name__ == "__main__":
