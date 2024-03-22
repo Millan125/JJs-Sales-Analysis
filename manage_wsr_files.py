d635b73499b6d3402be8822fc14b3cf1ab91713d
Millan125 <andyjones12722@gmail.com>
2024-03-21 21:42:43 -0400

    "week to path file is now a json, manage wsr files now excludes empty space at the beginning of keys and exlcudes repeat files by checking for ) in the string"

--- manage_wsr_files.py
+++ manage_wsr_files.py
@@ -3,40 +3,47 @@ def main():
     import json
     from fnmatch import fnmatch
 
-    #assign downloads directory path
+    # assign downloads directory path
     dir_path = r"C:\Users\milla\Downloads"
 
-    #create list to store files from downloads
+    # create list to store files from downloads
     are_files = []
     wsr_files = []
     new_dict = {}
 
     # check if current path is a file and add to are_files
     for path in os.listdir(dir_path):
-    
+
         if os.path.isfile(os.path.join(dir_path, path)):
             are_files.append(os.path.join(dir_path, path))
 
-    #check if files in are_files contain "Weekly Sales Report" and add those to wsr_files
+    # check if files in are_files contain "Weekly Sales Report" and add those to wsr_files
     for file in are_files:
-        if fnmatch(file, "*Weekly Sales Report*"):
+        if fnmatch(file, "*Weekly Sales Report*") and file[-5] != ")":
             wsr_files.append(file)
-    
-    
-    #create a dictionary 'Week #, -year-': path
+
+    # create a dictionary 'Week #, -year-': path
     for file in wsr_files:
-        key = file[-17:-4]
+        if file[-17] == " ":
+            key = file[-16:-4]
+        else:
+            key = file[-17:-4]
         new_dict[key] = file
 
-    #open dictionary file, check if keys exist, write to dict if they do not
-    with open('wsr_week_to_path.txt', "w+") as f:
-        data = dict(f.read())
+    # open dictionary file, check if keys exist, write to dict if they do not
+    with open("wsr_week_to_path.json", "w+") as f:
+        old_json_data = f.read()
+        if old_json_data:
+            data = json.loads(old_json_data)
+        else:
+            data = {}
 
         for x, y in new_dict.items():
             if x not in data.keys():
-                new_dict[x] = y
-                data.update(new_dict)
-        f.write(str(data))
-    
+                data[x] = y
+        new_json_data = json.dumps(data)
+        f.write(new_json_data)
+
+
 if __name__ == "__main__":
     main()
--- update_master.py
+++ update_master.py
@@ -1,28 +1,35 @@
-import pandas as pd
-from read_excel_write_df import rewd
+def um():
+    import pandas as pd
+    from read_excel_write_df import rewd
 
-# Obtain week:
-with open("wsr_week_to_path.txt", "r") as d:
-    path_dict = d.read()
+    # Obtain week:
+    with open("wsr_week_to_path.txt", "r") as d:
+        path_txt = d.read()
+        print(path_txt)
 
-with open("master_sales.csv", "r") as f:
-    master_csv = f.read()
+    # with open("master_sales.csv", "r") as f:
+    #     master_csv = f.read()
 
-if master_csv:
-    master_df = pd.read_csv(master_csv)
-else:
-    master_df = pd.DataFrame()
+    # if master_csv:
+    #     master_df = pd.read_csv(master_csv)
+    # else:
+    #     master_df = pd.DataFrame()
 
-while path_dict:
-    path = path_dict.popitem()
-    new_df = rewd(path[0], path[1])
-    new_master_df = pd.concat([master_df, new_df]).sort_inndex(inplace=True)
+    # while path_dict:
+    #     path = path_dict.popitem()
+    #     new_df = rewd(path[0], path[1])
+    #     new_master_df = pd.concat([master_df, new_df]).sort_inndex(inplace=True)
 
-new_master_csv = ""
-new_master_df.to_csv(new_master_csv, index=False)
+    # new_master_csv = ""
+    # new_master_df.to_csv(new_master_csv, index=False)
 
-with open("wsr_week_to_path.txt", "w") as f:
-    pass
+    # with open("wsr_week_to_path.txt", "w") as f:
+    #     pass
 
-with open("master_sales.csv", "w") as f:
-    f.write(new_master_csv)
+    # with open("master_sales.csv", "w") as f:
+    #     f.write(new_master_csv)
+    um()
+
+
+if __name__ == "__main__":
+    um()
