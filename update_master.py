80b1bb64dc16a6986ec53259ebaaadb61d372d21
Millan125 <59591714+Millan125@users.noreply.github.com>
2024-03-22 16:08:57 -0400

    Update update_master.py

--- update_master.py
+++ update_master.py
@@ -9,34 +9,27 @@ def um():
         path_json = d.read()
         path_dict = json.loads(path_json)
 
-    with open("wsr_week_to_path.json", "w") as f:
-        pass
-
-    # with open("master_sales.xls", "r") as f:
+    # with open("master_sales.xlsx", "r") as f:
     #     master_xls = f.read()
-    if os.path.getsize("master_sales.xls") != 0:
-        master_df = pd.read_excel("master_sales.xls")
+    if os.path.getsize("master_sales.xlsx") != 0:
+        master_df = pd.read_excel("master_sales.xlsx")
     else:
         master_df = pd.DataFrame()
-
-    # if master_xls:
-    #     master_df = pd.read_csv(master_xls)
-    # else:
-    #     master_df = pd.DataFrame()
 
     while path_dict:
         path = path_dict.popitem()
         new_df = rewd(path[0], path[1])
-        new_master_df = pd.concat([master_df, new_df]).sort_index(inplace=True)
+        new_master_df = pd.concat([master_df, new_df]).sort_index(inplace=False)
+        print(path)
+        # print(new_df.head())
+        print(new_master_df.head())
+    # with open("wsr_week_to_path.json", "w") as f:
+    #     pass
 
-    with open("wsr_week_to_path.json", "w") as f:
-        pass
+    new_master_df.to_excel("master_sales.xlsx", index=False)
 
-    new_master_df.to_excel("master_sales.xls", index=False)
-
-    # with open("master_sales.xls", "w") as f:
-    #     f.write(pd.to_csv(new_master_df))
-    um()
+    with open("master_sales.xlsx", "w") as f:
+        f.write(pd.to_csv(new_master_df))
 
 
 if __name__ == "__main__":
